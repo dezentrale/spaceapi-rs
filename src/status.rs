c29d3121bf75852f3b38f1f67837f2362614f961
tuxa <69870860+mrtuxa@users.noreply.github.com>
2023-02-21 11:36:24 +0000

    commit

--- src/status.rs
+++ src/status.rs
@@ -20,7 +20,7 @@ pub struct Location {
     Used if the space does not have a fixed location or does not want to publish
     the location
     **/
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "f64::is_none")]
     pub lat: f64,
     pub lon: f64,
     #[serde(skip_serializing_if = "Option::is_none")]
