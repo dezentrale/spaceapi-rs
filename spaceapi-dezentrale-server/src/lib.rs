303e0e77521e4be97287ccb73b563c8ffd1594fb
Alexander Böhm <alexander.boehm@malbolge.net>
2023-07-02 01:43:16 +0200

    Lowered privileges of CORS, code format

--- spaceapi-dezentrale-server/src/lib.rs
+++ spaceapi-dezentrale-server/src/lib.rs
@@ -8,7 +8,8 @@ use rocket::{
     fairing::{Fairing, Info, Kind},
     http::{
         hyper::header::{
-            ACCESS_CONTROL_ALLOW_CREDENTIALS, ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS,
+            ACCESS_CONTROL_ALLOW_HEADERS,
+            ACCESS_CONTROL_ALLOW_METHODS,
             ACCESS_CONTROL_ALLOW_ORIGIN,
         },
         ContentType, Header, Status,
@@ -127,7 +128,6 @@ pub struct SpaceConfig {
     pub admin: AdminConfig,
     #[serde(default, rename = "status_display")]
     pub status_display: StatusDisplayTypes,
-
 }
 
 impl SpaceConfig {
@@ -210,7 +210,11 @@ pub async fn index(
 ) -> (ContentType, String) {
     let name = &template.space;
     let logo = &template.logo;
-    let status = if space.is_open().await { &displays.text.open } else { &displays.text.closed };
+    let status = if space.is_open().await {
+        &displays.text.open
+    } else {
+        &displays.text.closed
+    };
 
     let html = format!(
         r#"<html>
@@ -243,14 +247,28 @@ pub async fn get_status_v14<'a>(
 }
 
 #[get("/status/text")]
-pub async fn get_status_text<'a>(space: &State<SpaceGuard>, displays: &State<StatusDisplayTypes>) -> (ContentType, String) {
-    let status = if space.is_open().await { displays.text.open.clone() } else { displays.text.closed.clone() };
+pub async fn get_status_text<'a>(
+    space: &State<SpaceGuard>,
+    displays: &State<StatusDisplayTypes>,
+) -> (ContentType, String) {
+    let status = if space.is_open().await {
+        displays.text.open.clone()
+    } else {
+        displays.text.closed.clone()
+    };
     (ContentType::Text, status)
 }
 
 #[get("/status/html")]
-pub async fn get_status_html<'a>(space: &State<SpaceGuard>, displays: &State<StatusDisplayTypes>) -> (ContentType, String) {
-    let status = if space.is_open().await { displays.html.open.clone() } else { displays.html.closed.clone() };
+pub async fn get_status_html<'a>(
+    space: &State<SpaceGuard>,
+    displays: &State<StatusDisplayTypes>,
+) -> (ContentType, String) {
+    let status = if space.is_open().await {
+        displays.html.open.clone()
+    } else {
+        displays.html.closed.clone()
+    };
     (ContentType::HTML, status)
 }
 
@@ -275,9 +293,8 @@ impl Fairing for Cors {
 
     async fn on_response<'a>(&self, _request: &'a Request<'_>, response: &mut Response<'a>) {
         // Allow GET from all locations
-        response.set_header(Header::new(ACCESS_CONTROL_ALLOW_CREDENTIALS.as_str(), "true"));
-        response.set_header(Header::new(ACCESS_CONTROL_ALLOW_HEADERS.as_str(), "*"));
-        response.set_header(Header::new(ACCESS_CONTROL_ALLOW_METHODS.as_str(), "*"));
+        response.set_header(Header::new(ACCESS_CONTROL_ALLOW_HEADERS.as_str(), "X-API-Key"));
+        response.set_header(Header::new(ACCESS_CONTROL_ALLOW_METHODS.as_str(), "POST,GET"));
         response.set_header(Header::new(ACCESS_CONTROL_ALLOW_ORIGIN.as_str(), "*"));
     }
 }
